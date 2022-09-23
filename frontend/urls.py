94b450e011a2d21184b64f6cd4e34d6b7c0d3e6d
xepoec7 <40522995+xepoec7@users.noreply.github.com>
2022-09-23 11:40:54 +0200

    Added menu pdf download

--- frontend/urls.py
+++ frontend/urls.py
@@ -8,4 +8,5 @@ app_name = "frontend"
 urlpatterns = [
     path('', views.home_page, name="home"),
     path('menu/', views.menu_page, name="menu"),
+    path('menu/pdf/', views.PdfView.as_view(), name="pdf"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from api.models import Category
+from django_renderpdf.views import PDFView
+from api.models import Category, Product
 from frontend.models import Footer_settings, Homepage_settings
 
 def home_page(request):
@@ -16,3 +17,27 @@ def menu_page(request):
         "footer": Footer_settings.objects.get(id=1)
     }
     return render(request, "menu.html", context)
+
+
+
+def pdf_page(request):
+    categories = Category.objects.all()
+    products = Product.objects.filter(on_menu=True)
+    ctx = {
+        "categories": categories,
+        "products": products
+    }
+    return render(request, "pdf.html", ctx)
+
+
+
+class PdfView(PDFView):
+    template_name = 'pdf.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context['categories'] = Category.objects.all()
+        context['products'] = Product.objects.filter(on_menu=True)
+
+        return context

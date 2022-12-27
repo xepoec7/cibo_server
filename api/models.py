1b91235c7d8212e15e54ac940215ad13520aaf11
xepoec7 <40522995+xepoec7@users.noreply.github.com>
2022-12-27 16:08:55 +0100

    models update

--- api/models.py
+++ api/models.py
@@ -13,8 +13,9 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=30, unique=True)
+    image = models.ImageField(upload_to='/product_images', blank=True, null=True)
     allergens = models.CharField(max_length=30, null=True, blank=True)
-    ingredient = models.TextField(blank=True, null=True) # Maybe Fields ??
+    ingredient = models.TextField(blank=True, null=True) 
     price = models.DecimalField(max_digits=8, decimal_places=2)
     on_menu = models.BooleanField(default=True, blank=True)
 
@@ -31,6 +32,11 @@ class Client(models.Model):
     ]
     client_type = models.CharField(max_length=3, choices=CLIENT_TYPES)
     name = models.CharField(max_length=80)
+    address = models.CharField(max_length=80, blank=True, null=True)
+    postal_code = models.CharField(max_length=12, blank=True, null=True)
+    city = models.CharField(max_length=80, blank=True, null=True)
+    tel = models.CharField(max_length=20, blank=True, null=True)
+    email = models.CharField(max_length=80, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.name
@@ -48,6 +54,7 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now=True, blank=True)
     due_time = models.DateTimeField(blank=True, null=True)
     status = models.CharField(max_length=1, choices=STATUS, default='O', blank=True)
+    total = models.DecimalField(max_digits=8, decimal_places=2, blank=True, default=0.00)
 
     def __str__(self) -> str:
         return str(self.id)
@@ -58,7 +65,6 @@ class OrderItem(models.Model):
     order = models.ForeignKey("Order", related_name='orderitems', on_delete=models.CASCADE)
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     qty = models.IntegerField(blank=True, default=1)
-    sum = models.DecimalField(max_digits=8, decimal_places=2, blank=True, default=0.00)
 
     def __str__(self) -> str:
         return f'{self.product} x{self.qty}'
